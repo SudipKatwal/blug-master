93063fd222702fe1e16ed7c38c4cbfb53d3f89cd
bikiroserai <hellobikashrai@gmail.com>
2018-04-01 17:44:10 +0545

    post logs addedd and view fixed

--- resources/views/Back/Pages/Dashboard/dashboard.blade.php
+++ resources/views/Back/Pages/Dashboard/dashboard.blade.php
@@ -7,7 +7,7 @@
         <!-- Content Header (Page header) -->
         <section class="content-header">
             <h1>
-                Dashboard
+                Admin Dashboard
             </h1>
              <!-- Info boxes -->
     <section class="content">
--- resources/views/Back/Pages/Posts/post-logs.blade.php
+++ resources/views/Back/Pages/Posts/post-logs.blade.php
@@ -1,14 +1,28 @@
 @extends('Back.Layouts.master')
 @section('content')
+
+
+    <!-- Content Wrapper. Contains page content -->
+    <div class="content-wrapper">
+        <!-- Content Header (Page header) -->
+        <section class="content-header">
+            <h1>
+                Post Logs
+            </h1>
+            
+        </section>
+
+        <!-- Main content -->
+        <section class="content">
 <div class="row">
         <div class="col-xs-12">
           <div class="box">
             <div class="box-header">
-              <h3 class="box-title">Responsive Hover Table</h3>
+              <h3 class="box-title">Details of Post History</h3>
 
               <div class="box-tools">
-                <div class="input-group input-group-sm" style="width: 150px;">
-                  <input type="text" name="table_search" class="form-control pull-right" placeholder="Search">
+                <div class="input-group input-group-sm" style="width: 300px;">
+                  <input type="text" name="table_search" class="form-control pull-right" placeholder="Search content by users name">
 
                   <div class="input-group-btn">
                     <button type="submit" class="btn btn-default"><i class="fa fa-search"></i></button>
@@ -21,38 +35,66 @@
               <table class="table table-hover">
                 <tr>
                   <th>ID</th>
-                  <th>User</th>
-                  <th>Date</th>
+                  <th>Writers</th>
+                  <th>Date of Submission</th>
                   <th>Status</th>
                   <th>Reason</th>
                 </tr>
                 <tr>
-                  <td>183</td>
+                  <td>1</td>
                   <td>John Doe</td>
                   <td>11-7-2014</td>
                   <td><span class="label label-success">Approved</span></td>
-                  <td>Bacon ipsum dolor sit amet salami venison chicken flank fatback doner.</td>
+                  <td>Everything is good</td>
                 </tr>
                 <tr>
-                  <td>219</td>
+                  <td>2</td>
                   <td>Alexander Pierce</td>
                   <td>11-7-2014</td>
                   <td><span class="label label-warning">Pending</span></td>
-                  <td>Bacon ipsum dolor sit amet salami venison chicken flank fatback doner.</td>
+                  <td>Content are copied from other sites.</td>
                 </tr>
                 <tr>
-                  <td>657</td>
+                  <td>3</td>
                   <td>Bob Doe</td>
                   <td>11-7-2014</td>
                   <td><span class="label label-primary">Approved</span></td>
-                  <td>Bacon ipsum dolor sit amet salami venison chicken flank fatback doner.</td>
+                  <td>Nice content!</td>
                 </tr>
                 <tr>
-                  <td>175</td>
+                  <td>4</td>
                   <td>Mike Doe</td>
                   <td>11-7-2014</td>
-                  <td><span class="label label-danger">Denied</span></td>
-                  <td>Bacon ipsum dolor sit amet salami venison chicken flank fatback doner.</td>
+                  <td><span class="label label-danger">Pending</span></td>
+                  <td>Content is too short</td>
+                </tr>
+                <tr>
+                  <td>1</td>
+                  <td>John Doe</td>
+                  <td>11-7-2014</td>
+                  <td><span class="label label-success">Approved</span></td>
+                  <td>Everything is good</td>
+                </tr>
+                <tr>
+                  <td>2</td>
+                  <td>Alexander Pierce</td>
+                  <td>11-7-2014</td>
+                  <td><span class="label label-warning">Pending</span></td>
+                  <td>Content are copied from other sites.</td>
+                </tr>
+                <tr>
+                  <td>3</td>
+                  <td>Bob Doe</td>
+                  <td>11-7-2014</td>
+                  <td><span class="label label-primary">Approved</span></td>
+                  <td>Nice content!</td>
+                </tr>
+                <tr>
+                  <td>4</td>
+                  <td>Mike Doe</td>
+                  <td>11-7-2014</td>
+                  <td><span class="label label-danger">Pending</span></td>
+                  <td>Content is too short</td>
                 </tr>
               </table>
             </div>
@@ -62,6 +104,70 @@
         </div>
       </div>
     </section>
-    <!-- /.content -->
-  </div>
-  @endsection
+    <section class="content">
+      <div class="row">
+        <div class="col-md-3 col-sm-6 col-xs-12">
+          <div class="info-box">
+            <span class="info-box-icon bg-green"><i class="fa fa-check-circle"></i></span>
+
+            <div class="info-box-content">
+              <span class="info-box-text">Approved posts</span>
+              <h2>434</h2>
+              {{-- <span class="info-box-number">37</span> --}}
+            </div>
+            <!-- /.info-box-content -->
+          </div>
+          <!-- /.info-box -->
+        </div>
+        <!-- /.col -->
+        <div class="col-md-3 col-sm-6 col-xs-12">
+          <div class="info-box">
+            <span class="info-box-icon bg-blue"><i class="fa fa-hourglass-2"></i></span>
+
+            <div class="info-box-content">
+              <span class="info-box-text">Pending Posts</span>
+              <h2>44</h2>
+            </div>
+            <!-- /.info-box-content -->
+          </div>
+          <!-- /.info-box -->
+        </div>
+        <!-- /.col -->
+
+        <!-- fix for small devices only -->
+        <div class="clearfix visible-sm-block"></div>
+
+        <div class="col-md-3 col-sm-6 col-xs-12">
+          <div class="info-box">
+            <span class="info-box-icon bg-red"><i class="fa fa-close"></i></span>
+
+            <div class="info-box-content">
+              <span class="info-box-text">Rejected posts</span>
+             <h2>434</h2>
+            </div>
+            <!-- /.info-box-content -->
+          </div>
+          <!-- /.info-box -->
+        </div>
+        <!-- /.col -->
+        <div class="col-md-3 col-sm-6 col-xs-12">
+          <div class="info-box">
+            <span class="info-box-icon bg-yellow"><i class="fa fa-calendar-check-o"></i></span>
+
+            <div class="info-box-content">
+              <span class="info-box-text">Assigned Posts</span>
+             <h2>34</h2>
+            </div>
+            <!-- /.info-box-content -->
+          </div>
+          <!-- /.info-box -->
+        </div>
+        <!-- /.col -->
+      </div>
+        </section>
+           
+        <!-- /.content -->
+    </div>
+    <!-- /.content-wrapper -->
+
+@endsection
